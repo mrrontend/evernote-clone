9f9b0df1089f67dcabd3fc8ab439ed53342e7a4a
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-15 23:20:25 +0530

    Added firebase Onsnapshot listener

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React, { Component } from "react";
 
+const firebase = require("firebase");
+
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    };
+  }
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection("notes")
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data();
+          data["id"] = _doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({ notes: notes });
+      });
+  };
   render() {
     return <div>App</div>;
   }
