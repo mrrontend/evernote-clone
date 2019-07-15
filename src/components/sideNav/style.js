003d33e0d93be059c8909f6abcce0c59881c6d83
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-16 02:02:44 +0530

    Side nav styles

--- src/components/sideNav/style.js
+++ src/components/sideNav/style.js
@@ -1,7 +1,33 @@
-import { textAlign } from "@material-ui/system";
-
 const style = theme => ({
-  sidecontainer: {
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    height: "calc(100% - 35px)",
+    position: "absolute",
+    left: "0",
+    width: "300px",
+    boxShadow: "0px 0px 2px black"
+  },
+  newChatBtn: {
+    borderRadius: "0px"
+  },
+  unreadMessage: {
+    color: "red",
+    position: "absolute",
+    top: "0",
+    right: "5px"
+  },
+  newNoteBtn: {
+    width: "100%",
+    height: "35px",
+    borderBottom: "1px solid black",
+    borderRadius: "0px",
+    backgroundColor: "#29487d",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "#88a2ce"
+    }
+  },
+  sidebarContainer: {
     marginTop: "0px",
     width: "300px",
     height: "100%",
@@ -10,17 +36,23 @@ const style = theme => ({
     overflowY: "scroll",
     overflowX: "hidden"
   },
-  sideNewbutton: {
+  newNoteInput: {
     width: "100%",
-    textAlign: "center",
+    margin: "0px",
     height: "35px",
-    borderBottom: "1px solid black",
-    borderRadius: "0px",
-    backgroundColor: "green",
-    color: "white",
-    "&:hover": {
-      backgroundColor: "#88a2ce"
+    outline: "none",
+    border: "none",
+    paddingLeft: "5px",
+    "&:focus": {
+      outline: "2px solid rgba(81, 203, 238, 1)"
     }
+  },
+  newNoteSubmitBtn: {
+    width: "100%",
+    backgroundColor: "#28787c",
+    borderRadius: "0px",
+    color: "white"
   }
 });
+
 export default style;
